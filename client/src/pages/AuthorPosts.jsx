cd71026a092ba742cb80f1d5c3309c435051a290
Kavindi Vithanage <kavindivithanage23@gmail.com>
2024-03-17 03:36:21 +0530

    Updated

--- client/src/pages/AuthorPosts.jsx
+++ client/src/pages/AuthorPosts.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DUMMY_POSTS } from '../data'
+
+import PostItem from '../components/PostItem'
 
 const AuthorPosts = () => {
+
+  const [posts, setPosts] = useState(DUMMY_POSTS)
+
   return (
-    <div>AuthorPosts</div>
+    // <div>AuthorPosts</div>
+
+    <section className="posts"> 
+
+      {posts.length > 0 ?  <div className="container posts__container">
+       {
+        posts.map(({id, thumbnail, category, title, description, authorID}) => 
+        <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} 
+        description={description} authorID={authorID} />)
+       }
+       </div> : <h2 className='center'>No Posts Found</h2>}
+
+    </section>
   )
 }
 
--- client/src/pages/CategoryPosts.jsx
+++ client/src/pages/CategoryPosts.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { DUMMY_POSTS } from '../data'
 
-const Categoryposts = () => {
+import PostItem from '../components/PostItem'
+
+const CategoryPosts = () => {
+
+  const [posts, setPosts] = useState(DUMMY_POSTS)
+
   return (
-    <div>Categoryposts</div>
+    // <div>AuthorPosts</div>
+
+    <section> 
+
+      {posts.length > 0 ?  <div className="container posts__container">
+       {
+        posts.map(({id, thumbnail, category, title, description, authorID}) => 
+        <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} title={title} 
+        description={description} authorID={authorID} />)
+       }
+       </div> : <h2 className='center'>No Posts Found</h2>}
+
+    </section>
   )
 }
 
-export default Categoryposts
+export default CategoryPosts
