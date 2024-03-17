d776bcbc61e27b7586cbc57fa5379493f9b05298
Kavindi Vithanage <kavindivithanage23@gmail.com>
2024-03-17 12:39:46 +0530

    Frontend Content Updated

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const UserProfile = () => {
   return (
-    <div>UserProfile</div>
+    // <div>UserProfile</div>
+    <section className="profile">
+
+      <div className="container profile__container">
+        <Link to={`myposts/sdfsdf`} className='btn'> My Posts</Link>
+
+        <div className="profile__details">
+          
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
